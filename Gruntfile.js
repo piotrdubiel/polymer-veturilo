03670760f4e5d2bc44bbd117368241a3a2ac9593
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-08-15 00:00:38 +0200

    a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
 module.exports = function(grunt) {
 	var port = grunt.option('port') || 8000;
+    if (process.env.DEV) {
+        var base = "build/local/";
+    }
+    else {
+        var base = "build/remote/";
+    }
+
+    console.log("Base build folder: " + base);
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),        
@@ -7,49 +15,68 @@ module.exports = function(grunt) {
 			server: {
 				options: {
 					port: port,
-					base: 'build/'
-				}
+					base: base
+                }
 			}
 		},
 	    watch: {
 			main: {
-				files: ['elements/**/*.html', 'elements/**/*.css', 'elements/**/*.js', 'components/**/*.html', 'components/**/*.css', 'components/**/*.js', '*.html', '*.css'],
-                tasks: ['vulcanize'],                                                                                                                                                      
+				files: ['elements/**/*.html',
+                        'elements/**/*.css',
+                        'elements/**/*.js',
+                        'components/**/*.html',
+                        'components/**/*.css',
+                        'components/**/*.js',
+                        '*.html',
+                        '*.css',
+                        'Gruntfile.*'],
+                tasks: ['vulcanize', 'copy'],                                                                                                                                                      
                 options: {
                     atBegin: true
                 },
 			},
             build: {
-                files: ['build/*'],
+                files: ['build/**/*'],
                 options: {
                     livereload: true
                 }
             }
 		},
         vulcanize: {
-            default: {
+            remote: {
                 options: {
-                    inline: true
+                    inline: true,
+                    csp: true
                 },
                 files: {
-                    'build/index.html': 'index.html'
+                    'build/remote/index.html': 'remote.html'
                 }
             },
             base: {
                 options: {
                     csp: true,
-                    inline: true,
-                    strip: true
+                    inline: true
                 },
                 files: {
-                    'build/stations.html': 'base.html'
+                    'build/base/stations.html': 'base.html'
                 }
+            }
+        },
+        copy: {
+            local: {
+                files: [
+                    {expand: true, src: ['elements/**/*'], dest: 'build/local/'},
+                    {expand: true, src: ['data/*'], dest: 'build/local/elements/'},
+                    {expand: true, src: ['components/**/*'], dest: 'build/local/'},
+                    {src: ['local.html'], dest: 'build/local/index.html'}
+                ]
             }
         }
     });
 	
-    grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-vulcanize');
 
 	grunt.registerTask('default', ['connect', 'watch']);
